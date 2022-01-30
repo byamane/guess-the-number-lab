a6118638078cb4a4e9f3c5d3a6a373aff281b0ad
byamane <byamane21@gmail.com>
2022-01-30 16:07:31 -0600

    complete task 3

--- app.js
+++ app.js
@@ -20,18 +20,25 @@ const game = {
   secretNum: null,
   prevGuesses: [],
   getGuess: function(){
-    console.log(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+    let userGuess = +prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`);
+    // Setting userGuess to prompt user to input number between 1 and 100. The parseInt() function will point userGuess to NaN if the user inputs a guess that is not a number
+    // Next, I want to incorporate a while loop that will run as many times needed while the user gives an incorrect guess that is not a number or within the upper/lower thresholds
+    while(isNaN(userGuess)|| userGuess < this.smallestNum ||userGuess > this.biggestNum){
+        userGuess = +prompt(`Guess must be a number between ${this.smallestNum} and ${this.biggestNum}. Please try again.`)
+      }
   },
+
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    
+    // for(let i = 0;)
 
       return this.secretNum
-      // this.prevGuesses += 1; // keeping running track of number of user's previous guesses
+      this.prevGuesses += 1; // keeping running track of number of user's previous guesses
   }
 }
 
 game.getGuess();
-// console.log(game.play());
-// console.log(game.prevGuesses.length);
-
+console.log(game.play());
+console.log(game.prevGuesses.length);
